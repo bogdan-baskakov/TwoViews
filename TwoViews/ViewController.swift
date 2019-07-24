bf2ac513d5f18224009b5f69ffcff2ba43c97b34
Bogdan Baskakov <bgdnbskkv@gmail.com>
2019-07-24 14:51:42 +0500

    Initial Commit

---
+++ TwoViews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwoViews
+//
+//  Created by Богдан Баскаков on 23/07/2019.
+//  Copyright © 2019 Bogdan Baskakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
